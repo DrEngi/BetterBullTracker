d024bd3cdc80d74290648d79cf1e6bdff80806e4
Dr. Engineer <6692751+DrEngi@users.noreply.github.com>
2021-01-15 00:18:04 -0500

    creation of geocoding controller

---
+++ BetterBullTracker/APIControllers/GeocodingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BetterBullTracker.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeocodingController : ControllerBase
+    {
+        //TODO: Use Mongo collection for buildings
+    }
+}
